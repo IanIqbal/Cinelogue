22b8e7e8f50743da83f7ab9004cad47d3ae66dbf
ian <ianiqball@gmail.com>
2026-01-21 13:56:19 +0700

    feat: tidy up search impelementation

--- cinelogue-web/src/views/SearchPage.jsx
+++ cinelogue-web/src/views/SearchPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { getMoviesByCategory, getMoviesGenres, getSearchResult } from "../store/action"
+import {  getSearchResult } from "../store/action"
 import CardRow from "../components/Cardrow"
 import "./Home.css"
 import Loading from "../components/Loading"
@@ -9,12 +9,11 @@ import { useSearchParams } from "react-router-dom"
 export default function SearchPage() {
     const dispatch = useDispatch()
     const [page, setPage] = useState(1)
-    const [loading, setLoading] = useState(true)
     const searchResult = useSelector((state) => state.mainSlice.searchResult)
     const [searchParams, setSearchParams] = useSearchParams()
+    const isSearchLoading = useSelector((state) => state.mainSlice.isSearchLoading)
     function handleScroll() {
-            if (window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight) {
-                setLoading(true)
+            if ( !isSearchLoading && window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight) {
                 setPage(prev => prev + 1)
             }
     }
@@ -22,14 +21,10 @@ export default function SearchPage() {
     useEffect(()=>{
         setPage(1)
     },[searchParams.get("query")])
+
+  
     useEffect(() => {
-
-        setTimeout(() => {
-            setLoading(false)
-        }, 3000)
-
         dispatch(getSearchResult(searchParams.get("query"), page))
-
     }, [page])
     useEffect(() => {
         window.addEventListener("scroll", handleScroll)
@@ -44,8 +39,8 @@ export default function SearchPage() {
             <CardRow items={searchResult.results} ></CardRow>
             <div style={{ display: "flex", justifyContent: "center" }} >
 
-                {loading && searchResult.page < searchResult.total_pages &&  <Loading></Loading>}
-                {searchResult.page >= searchResult.total_pages && searchParams.get("query") &&<h3>All data for "{searchParams.get("query")}" has been displayed</h3>}
+                {isSearchLoading && searchResult.page < searchResult.total_pages &&  <Loading></Loading>}
+                {searchResult.results.length > 0 &&searchResult.page >= searchResult.total_pages && searchParams.get("query") &&<h3>All data for "{searchParams.get("query")}" has been displayed</h3>}
             </div>
         </div>
     )
